9c1ca72fb7679ee46cd0454b835ddaddcd59bc25
guedesclaudio <cgsarmentosilva@gmail.com>
2022-07-21 17:06:38 -0300

    Ajustes inputs

--- src/Components/WelcomeScreen.js
+++ src/Components/WelcomeScreen.js
@@ -10,16 +10,20 @@ export default function WelcomeScreen({
     setUserValueSelect
 }) {
 
-    const [messageErrorInput, setMessageErrorInput] = React.useState("hidden")
-    const [messageErrorSelect, setMessageErrorSelect] = React.useState("hidden")
+    const [messageError, setMessageError] = React.useState("")
+    //const [messageErrorSelect, setMessageErrorSelect] = React.useState("hidden")
+    const [showInput, setShowInput] = React.useState("")
+    const [showSelect, setShowSelect] = React.useState("hidden")
     
     function startsGame () {
         if (userValueInput > 4 || userValueInput < 1 || isNaN(userValueInput) ) {
-            setMessageErrorInput("error")
+            setMessageError("Digite entre 1 e 4")
             return
         }
+        setShowInput("hidden")
+        setShowSelect("")
         if (userValueSelect === "" ) {
-            setMessageErrorSelect("error")
+            setMessageError("Escolha um deck de cards")
             return
         }
         setHidesHome("home hidden")
@@ -28,24 +32,23 @@ export default function WelcomeScreen({
 
     const getInputValueInput = (event) => {
         setUserValueInput(Number(event.target.value)) 
-        setMessageErrorInput("hidden")
+        setMessageError("")
         console.log(userValueInput)
     }
 
     const getInputValueSelect = (event) => {
         setUserValueSelect(event.target.value)
-        setMessageErrorSelect("hidden")
+        setMessageError("")
     }
 
     return (
         <div className = {hidesHome}>
             <img src = "./img/lightning.png" />
             <h1>ZapRecall</h1>
-            <div className = "error"><p className = {messageErrorInput}>Digite um valor entre 1 e 4</p></div>
-            <input type = "text" placeholder = "Digite a sua meta de zaps" onChange = {getInputValueInput}/>
-            <div className = "error"><p className = {messageErrorSelect}>Escolha um deck de cards</p></div>
-            <select onChange = {getInputValueSelect}>
-                <option value = "" disabled selected>Escolha seu deck</option>
+            <div className = "error"><p>{messageError}</p></div>
+            <input className = {showInput} type = "text" placeholder = "Digite a sua meta de zaps" onChange = {getInputValueInput}/>
+            <select className = {showSelect} onChange = {getInputValueSelect}>
+                <option value = "" disabled selected >Escolha seu deck</option>
                 <option value = "JSX">JSX</option>
                 <option value = "CSS">HTML</option>
             </select>
